b6c1e71b2e889fa393c002928738977406ebab05
Liam Harwood <liamrharwood@gmail.com>
2015-12-04 22:11:12 -0500

    added another activity

---
+++ app/src/main/java/com/gitgud/hackathon/DisplayEventActivity.java
@@ -0,0 +1,20 @@
+package com.gitgud.hackathon;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DisplayEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        setContentView(R.layout.activity_display_event);
+    }
+
+}
